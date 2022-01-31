2e5961579201b7ab3124a5887a2d5d2ace0bf8c3
Mark James <55639318+mark-james-coder@users.noreply.github.com>
2022-01-31 19:47:06 +0000

    add deduct method to oystercard

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,12 +2,20 @@ class Oystercard
 
   attr_reader :balance
 
+  BALANCE_LIMIT = 90
+
   def initialize
     @balance = 0
   end
 
   def top_up(amount)
+    fail "Top up failed. Maximum allowed balance is £#{BALANCE_LIMIT}." if @balance + amount > BALANCE_LIMIT
     @balance += amount
+  end
+
+  def deduct(amount)
+    fail "Deduct failed. Minimum allowed balance is £0." if @balance - amount < 0
+    @balance -= amount
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -7,7 +7,19 @@ describe Oystercard do
   end
   describe '#top_up' do
     it 'increases the balance by specified amount' do
-    expect{ subject.top_up 5 }.to change{ subject.balance }.by 5
+      expect{ subject.top_up 5 }.to change{ subject.balance }.by 5
+    end
+    it "raises an error if topping up would take balance over balance limit" do
+      expect { subject.top_up 95}.to raise_error "Top up failed. Maximum allowed balance is £#{Oystercard::BALANCE_LIMIT}."
+    end
+  end
+  describe '#deduct' do
+    it 'decreases the balance by specified amount' do
+      subject.top_up(10)
+      expect{ subject.deduct 5 }.to change{ subject.balance }.by -5
+    end
+    it "raises an error if deducting would take balance under zero balance" do
+      expect { subject.deduct 5}.to raise_error "Deduct failed. Minimum allowed balance is £0."
     end
   end
 
